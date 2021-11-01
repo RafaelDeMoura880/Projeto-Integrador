a4a18ea32e980bd7717eb5326793f019163626bb
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2021-11-01 14:31:41 -0300

    Added player horizontal movement and sprite flip

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D playerRb;
+    Animator playerAnim;
+
+    float playerMovement;
+    [SerializeField] float playerSpeed = 1;
+
+    bool turnedRight = true;
+    [SerializeField] bool isOnGround;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        Vector2 pos = Camera.main.WorldToViewportPoint(transform.position);
+        playerMovement = Input.GetAxis("Horizontal");
+
+        if ((pos.x > 0 && playerMovement < 0) || (pos.x < 1 && playerMovement > 0))
+            playerMovement *= playerSpeed;
+        else
+            playerMovement = 0f;
+
+        if ((playerMovement > 0 && !turnedRight) || (playerMovement < 0 && turnedRight))
+            Flip();
+
+        if (playerMovement != 0)
+            playerAnim.SetBool("isRunning", true);
+        else
+            playerAnim.SetBool("isRunning", false);
+
+        playerRb.velocity = new Vector2(playerMovement, playerRb.velocity.y);
+    }
+
+    void Flip()
+    {
+        turnedRight = !turnedRight;
+
+        Vector3 auxScale = this.transform.localScale;
+        auxScale.x *= -1;
+        this.transform.localScale = auxScale;
+    }
+}
