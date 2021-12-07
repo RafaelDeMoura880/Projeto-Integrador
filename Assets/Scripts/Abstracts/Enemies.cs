6e630da91b4fcd8dfbe18a3a0e2423bd80204b68
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2021-12-07 17:29:23 -0300

    Added Enemies abstract script // enemy _blue script // _blue attack animation

---
+++ Assets/Scripts/Abstracts/Enemies.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemies : MonoBehaviour
+{
+    public float enemyMovement;
+    public float enemySpeed;
+
+    public virtual void Start()
+    {
+        enemyMovement = 0f;
+        enemySpeed = 0f;
+    }
+}
---
+++ Assets/Scripts/Enemy/_Blue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Blue : Enemies
+{
+    BoxCollider2D playerTrigger;
+    Animator enemyBlueAnim;
+    
+    public override void Start()
+    {
+        enemyMovement = 5f;
+        enemySpeed = 5f;
+
+        playerTrigger = this.transform.GetChild(0).GetComponent<BoxCollider2D>();
+        enemyBlueAnim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            enemyBlueAnim.SetBool("isOnTrigger", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            enemyBlueAnim.SetBool("isOnTrigger", false);
+        }
+    }
+}
