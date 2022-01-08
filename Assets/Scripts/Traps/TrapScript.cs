0a2b1bdf15b5fbdc64b3c2d90839f5ec473c1617
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-01-08 20:04:40 -0300

    Added simple trap damage script

---
+++ Assets/Scripts/Traps/TrapScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapScript : Enemies
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Dano(30);
+    }
+}
