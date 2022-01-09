a3e994a8fd60181917e84f568c6407b8a3799c4e
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-01-08 21:39:16 -0300

    Added lockscript

---
+++ Assets/Scripts/Scenario/LockScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockScript : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (PlayerScript.instance.hasKey)
+        {
+            PlayerScript.instance.hasKey = false;
+            CanvasScript.instanceCanvas.gameObject.transform.GetChild(3).gameObject.SetActive(false);
+            Destroy(this.gameObject);
+        }
+    }
+}
