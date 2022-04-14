4e22226b468cb6393167f8978e460f5f92004b90
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-04-14 20:12:44 -0300

    Adding more tiles on top level

--- Assets/Scripts/Collectibles/KeyScript.cs
+++ Assets/Scripts/Collectibles/KeyScript.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class KeyScript : Collectibles
 {
+    [SerializeField] int keyValue = 1;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player" && PlayerScript.instance.Money == 1)
+        if(collision.gameObject.tag == "Player" && PlayerScript.instance.Money == keyValue)
         {
             PlayerScript.instance.hasKey = true;
-            PlayerScript.instance.Money--;
+            PlayerScript.instance.Money -= keyValue;
             CanvasScript.instanceCanvas.transform.GetChild(3).gameObject.SetActive(true);
             Destroy(this.gameObject);
         }
